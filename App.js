981d3a1fc222db62e02ac558ca3c836b5d6118c1
furqan_ashraf <furqan_ashraf@outlook.com>
2020-11-29 21:23:56 +0500

    romved node module folder

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 let request = require("request");
 
-let apiKey = "0cf0be7893b5207a72d08d4fab3cd186";
-let city = "lahore";
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+// let apiKey = "0cf0be7893b5207a72d08d4fab3cd186";
+// let city = "lahore";
+// let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
+// request({ url: url, json: true }, function (err, response, body) {
+//   const { weather, main } = body;
+//   console.log(weather[0].main, main);
+// });
+const url =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZnVycWFuMDkiLCJhIjoiY2tpMzhkdjluMDI4dTJxbnU2eDBycGhhdCJ9.0dvYSFgJDcMR03CnioJx3Q&limit=1";
 request({ url: url, json: true }, function (err, response, body) {
-  const { weather, main } = body;
-  console.log(weather[0].main, main);
+  //   const { weather, main } = body;
+  console.log(`${response.body.features[0].center[0]} longitude`);
+  console.log(`${response.body.features[0].center[1]} latitude`);
 });
